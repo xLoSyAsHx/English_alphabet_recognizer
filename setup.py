864a14a9cae5172f3d6fadb4fc2611f6c5091d06
Ivan, Losev <ivan.s.losev@mail.ru>
2020-02-24 14:38:16 +0300

    Added setup and test scripts. Configured travis

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import alphabet_recogniser
+
+with open('README.md', 'r') as f:
+   LONG_DESCRIPTION = f.read()
+
+CLASSIFIERS = [
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'License :: Free for non-commercial use',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(
+    name='alphabet_recogniser',
+    version=alphabet_recogniser.__version__,
+    author="xLoSyAsHx",
+    author_email="https://github.com/xLoSyAsHx",
+    url='https://github.com/xLoSyAsHx/HSE_ML_alphabet_recognition',
+    description='Alphabet recognition model and utilities',
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+    keywords="alphabet recognition example examples",
+    test_suite='tests',
+    python_requires='>=3.7.*',
+    install_requires=[
+        'torch>=1.4.0',
+        'torchvision>=0.5.0'
+    ],
+    platforms=['Windows 10'],
+    license='NO'
+)
---
+++ tests.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+class AlphabetRecogniserTestCases(TestCase):
+    def test_addition1(self):
+        self.assertEqual(1 + 1, 2)
+    def test_addition2(self):
+        self.assertEqual(1 + 2, 3)
