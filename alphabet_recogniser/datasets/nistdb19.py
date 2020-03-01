ff02c9d61776294de35307765025aaa41b3e15ec
Ivan, Losev <ivan.s.losev@mail.ru>
2020-03-01 17:44:46 +0300

    Added save/load dataset functionality
    - fixed _process function
    - Update setup.py

--- alphabet_recogniser/datasets/nistdb19.py
+++ alphabet_recogniser/datasets/nistdb19.py
@@ -1,4 +1,5 @@
 import os
+import compress_pickle
 import torch.utils.data as data
 from PIL import Image
 from skimage import io
@@ -16,15 +17,14 @@ class NISTDB19Dataset(data.Dataset):
     url = 'https://s3.amazonaws.com/nist-srd/SD19/by_class.zip'
     md5_hash = '79572b1694a8506f2b722c7be54130c4'
     arch_name = 'by_class.zip'
-    data = []
-    targets = []
     folder_map = {
         'digits': {'start': 0x30, 'len': 10},
         'cap_letters': {'start': 0x41, 'len': 26},
         'low_letters': {'start': 0x61, 'len': 26},
     }
 
-    def __init__(self, root_dir=None, train=True, download=False, data_type=None, size_limit=1000, transform=None):
+    def __init__(self, root_dir=None, train=True, download=False, data_type=None,
+                 size_limit=1000, size_limit_per_class = True, transform=None):
         """
         Args:
             root_dir (string): Path to directory with 'by_class' folder
@@ -36,20 +36,26 @@ class NISTDB19Dataset(data.Dataset):
                 downloaded again.
             data_type (string): Data type for loading
                 can be 'digits', 'cap_letters' or 'low_letters'.
+            size_limit (int): Dataset global size limit to load
+            size_limit_per_class (bool): If True - size_limit will apply to every class,
+                otherwise, size limit for every class will be calculated as size_limit / num_classes
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
 
-        self.root_dir = os.path.expanduser(root_dir)
-        self.train = train
-        self.size_limit = size_limit
-        self.transform = transform
-
         if not self.folder_map.get(data_type):
             raise RuntimeError("Argument 'data_type' should be one of: \n  " +
                                repr(tuple(self.folder_map.keys())))
-
+        self.data = []
+        self.targets = []
         self.data_type = data_type
+        self.root_dir = os.path.expanduser(root_dir)
+        self.train = train
+        self.transform = transform
+        self.size_per_class = size_limit
+
+        if not size_limit_per_class:
+            self.size_per_class /= self.folder_map[data_type]['len']
 
         if os.path.exists(self.root_dir):
             self._process(download, data_type)
@@ -66,6 +72,28 @@ class NISTDB19Dataset(data.Dataset):
         target = self.targets[idx]
 
         return img, target
+
+    @staticmethod
+    def save_to_file(dataset, filepath, force_overwrite=False):
+        if not isinstance(dataset, NISTDB19Dataset):
+            raise RuntimeError(f"Object {type(dataset)} is not inherit from NISTDB19Dataset")
+
+        if not os.path.exists(os.path.dirname(filepath)):
+            raise RuntimeError(f"Folder {os.path.dirname(filepath)} does not exist")
+
+        if not force_overwrite and os.path.exists(filepath):
+            print(
+                f"\n[WARNING]: Can't save to '{filepath}'.\nFile already exist. Add 'force_overwrite=False' for overwrite")
+            return
+        with open(filepath, 'wb') as dataset_file:
+            compress_pickle.dump(dataset, dataset_file, compression="lzma")
+
+    @staticmethod
+    def load_from_file(filepath):
+        if not os.path.exists(filepath):
+            raise RuntimeError(f"Can't open {filepath}. File doesn't exist")
+        with open(filepath, 'rb') as dataset_file:
+            return compress_pickle.load(dataset_file, compression="lzma")
 
     def _download(self, need_to_download):
         arch_path = os.path.join(self.root_dir, self.arch_name)
@@ -85,17 +113,19 @@ class NISTDB19Dataset(data.Dataset):
 
         download_and_extract_archive(self.url, self.root_dir, filename=self.arch_name, md5=self.md5_hash)
 
-    def _add_samples_from_dir(self, img_dir_path, target):
+    def _add_samples_from_dir(self, img_dir_path, target, newly_added):
         if img_dir_path is None or target is None:
             raise RuntimeError("Image dir path and target must be valid. \nGiven path:",
                                img_dir_path, "\nGiven target:", target)
+
         for img_name in os.listdir(img_dir_path):
             img_path = os.path.join(img_dir_path, img_name)
             image = io.imread(img_path)
             self.data.append(image)
             self.targets.append(target)
+            newly_added += 1
 
-            if len(self.data) == self.size_limit:
+            if newly_added == self.size_per_class:
                 return 1
         return 0
 
@@ -107,18 +137,19 @@ class NISTDB19Dataset(data.Dataset):
             class_folder_name = hex(class_idx)[2:]
 
             if self.train:
+                newly_added = 0
                 for hsf_idx in range(0, 8):
                     hsf_name = 'hsf_' + str(hsf_idx)
                     path_to_img_dir = os.path.join(self.root_dir,
                                                    'by_class',
                                                    class_folder_name,
                                                    hsf_name)
-                    if self._add_samples_from_dir(path_to_img_dir, chr(class_idx)) == 1:
-                        return
+                    if self._add_samples_from_dir(path_to_img_dir, chr(class_idx), newly_added) == 1:
+                        break
             else:
                 path_to_img_dir = os.path.join(self.root_dir,
                                                 'by_class',
                                                class_folder_name,
                                                'train_' + class_folder_name)
-                if self._add_samples_from_dir(path_to_img_dir, chr(class_idx)) == 1:
-                    return
+                self._add_samples_from_dir(path_to_img_dir, chr(class_idx), 0)
+
--- alphabet_recogniser/train.py
+++ alphabet_recogniser/train.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+import alphabet_recogniser
 from alphabet_recogniser.datasets import NISTDB19Dataset
 
 import matplotlib.pyplot as plt
@@ -13,17 +14,31 @@ def imshow(img):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-print(torch.__version__)
+print(f"torch version: {torch.__version__}")
+print(f"torchvision version: {torchvision.__version__}")
+print(f"alphabet_recogniser version: {alphabet_recogniser.__version__}\n{'#'*40}")
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # ((mean1, mean2, ...), (std1, std2, ...))
 
-testset = NISTDB19Dataset(root_dir='./../data', data_type='low_letters', train=False, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(testset, batch_size=4,
+train_set = NISTDB19Dataset(root_dir='./../data', data_type='low_letters', train=False, download=True,
+                          transform=transform, size_limit=1000)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=4,
                                           shuffle=True, num_workers=0)
 
-dataiter = iter(trainloader)
+test_set = NISTDB19Dataset(root_dir='./../data', data_type='low_letters', train=False, download=True,
+                          transform=transform, size_limit=300)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=4,
+                                          shuffle=True, num_workers=0)
+
+
+
+
+dataiter = iter(train_loader)
 images, labels = dataiter.next()
 
 imshow(torchvision.utils.make_grid(images))
+
+
+
