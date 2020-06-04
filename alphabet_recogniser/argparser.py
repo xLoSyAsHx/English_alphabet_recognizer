45d4044ccc73baaf2ea36bf35e45ef2a8e0ef670
Ivan, Losev <ivan.s.losev@mail.ru>
2020-06-05 02:13:42 +0300

    Refactoring: argparser.py
    - Removed excess methods
    - Added checkers using argparse 'type' and 'choice'

--- alphabet_recogniser/argparser.py
+++ alphabet_recogniser/argparser.py
@@ -5,107 +5,106 @@ from alphabet_recogniser.datasets import NISTDB19Dataset
 
 
 class ArgParser:
-    __instance__ = None
+    __args__ = None
 
     def __positive_int__(value):
         ivalue = int(value)
         if ivalue <= 0:
-            raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+            raise argparse.ArgumentTypeError(f"{value} is an invalid positive int value")
         return ivalue
 
+    def __sys_path__(path):
+        if path is not None and not os.path.exists(path):
+            raise argparse.ArgumentTypeError(f"Path '{path}' doesn't exist")
+        return path
+
+    def __char_unique_array__(str):
+        if str is not None and (str[0] != '{' or str[-1] != '}' or ' ' in str):
+            raise argparse.ArgumentError("Invalid format of argument for '-classes'"
+                                         "Example: -classes {a,b,c}")
+        return str
+
     @staticmethod
-    def get_instance():
-        if ArgParser.__instance__ is None:
+    def get_args():
+        if ArgParser.__args__ is None:
             ArgParser()
-        return ArgParser.__instance__
+        return ArgParser.__args__
 
     def __init__(self):
         if ArgParser.__instance__ is not None:
             raise Exception("This class is a singleton!")
         else:
-            self.parser = argparse.ArgumentParser(
+            parser = argparse.ArgumentParser(
                 formatter_class=argparse.MetavarTypeHelpFormatter,
                 description="Neural Network for English alphabet recognizer. "
                             "Example: python -m alphabet_recogniser.train -root-dir ./data -data-type low_letters -train-limit 1000 -test-limit 300 -e 30 --use-preprocessed-data -classes {a,b,f}"
             )
 
             # Required options
-            self.parser.add_argument('-root-dir', type=str,  required=True,
+            parser.add_argument('-root-dir', type=str,  required=True,
                                      help="Path to data folder")
-            self.parser.add_argument('-e', type=ArgParser.__positive_int__, required=True,
+            parser.add_argument('-e', type=ArgParser.__positive_int__, required=True,
                                      help="Number of Epoches")
-            self.parser.add_argument('-batch-size', type=ArgParser.__positive_int__, required=True,
+            parser.add_argument('-batch-size', type=ArgParser.__positive_int__, required=True,
                                      help="batch_size value for DataLoader")
-            self.parser.add_argument('-data-type', type=str, choices=NISTDB19Dataset.folder_map.keys(), required=True,
+            parser.add_argument('-data-type', type=str, choices=NISTDB19Dataset.folder_map.keys(), required=True,
                                      help=f"Specify data type to use. Available types: {NISTDB19Dataset.folder_map.keys()}")
 
             # Model options
-            self.parser.add_argument('-m-save-path', type=str,
+            parser.add_argument('-m-load-path', type=ArgParser.__sys_path__,
+                                     help="Specify path to model for load")
+            parser.add_argument('-m-save-path', type=ArgParser.__sys_path__,
                                      help="Specify path to folder for save trained model")
-            self.parser.add_argument('-m-save-period', type=ArgParser.__positive_int__,
+            parser.add_argument('-m-save-period', type=ArgParser.__positive_int__,
                                      help="Specify how often save trained model")
 
             # Tensorboard options
-            self.parser.add_argument('-t-logdir', type=str,
+            parser.add_argument('-t-logdir', type=ArgParser.__sys_path__,
                                      default='./runs/',
                                      help="Specify path to tensorboard logs"
                                           "Example: -t-logdir ./runs/")
-            self.parser.add_argument('-t-images', type=ArgParser.__positive_int__,
-                                     help="Specify number of samples from dataset to upload to tensorboard"
-                                          "Must not exceed 100")
-            self.parser.add_argument('-t-cm-freq', type=ArgParser.__positive_int__,
+            parser.add_argument('-t-images', type=ArgParser.__positive_int__,
+                                     choices=range(0, 101), metavar="[0-100]",
+                                     help="Specify number of samples (0-100) from dataset to upload to tensorboard")
+            parser.add_argument('-t-cm-freq', type=ArgParser.__positive_int__,
                                      default=10,
                                      help="Specify how often to plot confusion matrix to upload to tensorboard")
-            self.parser.add_argument('-t-precision-bar-freq', type=ArgParser.__positive_int__,
+            parser.add_argument('-t-precision-bar-freq', type=ArgParser.__positive_int__,
                                      default=10,
                                      help="Specify how often to plot precision bar to upload to tensorboard")
-            self.parser.add_argument('-t-roc-auc-freq', type=ArgParser.__positive_int__,
+            parser.add_argument('-t-roc-auc-freq', type=ArgParser.__positive_int__,
                                      default=10,
                                      help="Specify how often to plot ROC curves to upload to tensorboard")
 
             # Dataset settings
-            self.parser.add_argument('-classes', type=str,
+            parser.add_argument('-classes', type=ArgParser.__char_unique_array__,
                                      help="Specify classes to use"
                                           "Example: -classes {a,b,c}")
-            self.parser.add_argument('-train-limit', type=ArgParser.__positive_int__,
+            parser.add_argument('-train-limit', type=ArgParser.__positive_int__,
                                      help="Specify total num of train samples"
                                           "         (use '--limit-per-class' for per class limitation)")
-            self.parser.add_argument('-test-limit', type=ArgParser.__positive_int__,
+            parser.add_argument('-test-limit', type=ArgParser.__positive_int__,
                                      help="Specify total num of samples"
                                           "         (use '--limit-per-class' for per class limitation)")
-            self.parser.add_argument('--use-preprocessed-data', action='store_true',
+            parser.add_argument('--use-preprocessed-data', action='store_true',
                                      help="Set 'use_preproc=True'")
-            self.parser.add_argument('--shuffle-train', action='store_true',
+            parser.add_argument('--shuffle-train', action='store_true',
                                      help="Set 'shuffle=True' for train dataset")
-            self.parser.add_argument('--shuffle-test', action='store_true',
+            parser.add_argument('--shuffle-test', action='store_true',
                                      help="Set 'shuffle=True' for test dataset")
 
             # For load/save data to/from lzima-zipped archive
-            self.parser.add_argument('-train-save-path', type=str,
+            parser.add_argument('-train-save-path', type=ArgParser.__sys_path__,
                                      help="Path to save lzima-zipped train dataset file"
                                           "Will use NISTDB19Dataset.save_to_file(dataset, path)")
-            self.parser.add_argument('-test-save-path', type=str,
+            parser.add_argument('-test-save-path', type=ArgParser.__sys_path__,
                                      help="Path to save lzima-zipped test dataset file"
                                           "Will use NISTDB19Dataset.save_to_file(dataset, path)")
-            self.parser.add_argument('-train-load-path', type=str,
+            parser.add_argument('-train-load-path', type=ArgParser.__sys_path__,
                                      help="Path to lzima-zipped train dataset file"
                                           "Will use NISTDB19Dataset.load_from_file(path)")
-            self.parser.add_argument('-test-load-path', type=str,
+            parser.add_argument('-test-load-path', type=ArgParser.__sys_path__,
                                      help="Path to lzima-zipped test dataset file"
                                           "Will use NISTDB19Dataset.load_from_file(path)")
 
-            ArgParser.__instance__ = self
-
-    def parse_args(self):
-        return self.parser.parse_args()
-
-    def check_compatibility(self, args):
-        if args.classes is not None and (args.classes[0] != '{' or args.classes[-1] != '}' or ' ' in args.classes):
-            raise argparse.ArgumentError("Invalid format of argument for '-classes'"
-                                         "Example: -classes {a,b,c}")
-
-        if args.t_images is not None and args.t_images > 100:
-            raise argparse.ArgumentError("Number of samples from dataset to upload must not exceed 100!")
-
-        if args.m_save_path is not None and not os.path.exists(args.m_save_path):
-            raise argparse.ArgumentError(f"Invalid path: parameter '-m-save-path' value '{args.m_save_path}'")
+            ArgParser.__args__ = parser.parse_args()
