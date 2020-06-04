95a69b9b292334ac9468c4c062a45ea2dce78e30
Ivan, Losev <ivan.s.losev@mail.ru>
2020-06-05 01:46:15 +0300

    Refactoring: train.py, utils.py
    - Moved tensorboard logic to tensorboard_utils.py
    - Replaced datasets.ustils to torch.datasets.utils.py
    - Moved getting data loaders to datasets.ustils.py
    - Changed Globals to Singleton "Config"

--- alphabet_recogniser/train.py
+++ alphabet_recogniser/train.py
@@ -1,135 +1,47 @@
 import os, sys, time, math
 import torch
-import torch.nn as nn
 import torch.optim as optim
 import torchvision
-import torchvision.transforms as transforms
-
-from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
-
 import numpy as np
-from datetime import datetime
 
 import alphabet_recogniser
-from alphabet_recogniser.datasets import NISTDB19Dataset
+from alphabet_recogniser.datasets.utils import NISTDB19Dataset_data_loaders
 from alphabet_recogniser.models import EngAlphabetRecognizer96
 from alphabet_recogniser.argparser import ArgParser
 from alphabet_recogniser.test import eval_cached
 
-from alphabet_recogniser.utils import upload_net_graph, save_model, add_logs_to_tensorboard
+from alphabet_recogniser.tensorboard_utils import upload_net_graph, save_model, add_logs_to_tensorboard, log
+from alphabet_recogniser.utils import Config
 
 
-class Globals:
-    def __init__(self):
-        self.args = None
-        self.writer = None
-        self.log_pref = None
-
-        self.train_transform = None
-        self.test_transform = None
-        self.classes = None
-        self.train_size_per_class = None
-        self.test_size_per_class = None
-
-        self.device = None
-        self.epoch_num = None
-        self.path_to_model = None
-G = Globals()
-
-
-def log(tag, text, glogal_step=0):
-    print(text)
-    G.writer.add_text(tag, text, glogal_step)
-
-
-def setup_global_vars():
-    G.train_size_per_class = G.args.train_limit
-    G.test_size_per_class = G.args.test_limit
-
-    G.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    G.epoch_num = G.args.e
-    G.path_to_model = 'cifar_net.torchmodel'
-
-    if G.args.t_cm_freq is None:
-        G.args.t_cm_freq = sys.maxsize
-
-    if G.args.t_precision_bar_freq is None:
-        G.args.t_precision_bar_freq = sys.maxsize
-
-    num_classes = len(G.args.classes[1:-1].split(',')) if G.args.classes is not None \
-             else NISTDB19Dataset.folder_map[G.args.data_type]['len']
-
-    G.log_pref = datetime.now().strftime('%Y_%B%d_%H-%M-%S')
-    G.writer = SummaryWriter(log_dir=f"{G.args.t_logdir}{G.log_pref}"
-                                     f"_e[{G.epoch_num}]"
-                                     f"_c[{num_classes}]"
-                                     f"_tr_s[{G.train_size_per_class if G.train_size_per_class is not None else 'All'}]"
-                                     f"_t_s[{G.test_size_per_class if G.test_size_per_class is not None else 'All'}]")
-
-
-def get_data_loaders(force_shuffle_test=True):
-    if hasattr(get_data_loaders, 'train'):
-        return get_data_loaders.train, get_data_loaders.test
-
-    if G.args.use_preprocessed_data:
-        NISTDB19Dataset.download_and_preprocess(root_dir=G.args.root_dir, data_type=G.args.data_type,
-                                                str_classes=G.args.classes)
-
-    if G.args.train_load_path is None:
-        train_set = NISTDB19Dataset(root_dir=G.args.root_dir, data_type=G.args.data_type, train=True, download=True,
-                                    str_classes=G.args.classes, use_preproc=G.args.use_preprocessed_data,
-                                    train_transform=G.train_transform, test_transform=G.test_transform,
-                                    size_limit=G.args.train_limit)
-        if G.args.train_save_path is not None:
-            NISTDB19Dataset.save_to_file(train_set, G.args.train_save_path)
-    else:
-        train_set = NISTDB19Dataset.load_from_file(G.args.train_load_path)
-    get_data_loaders.train = DataLoader(train_set, batch_size=G.args.batch_size,
-                                        shuffle=G.args.shuffle_train, num_workers=0)
-
-    if G.args.test_load_path is None:
-        test_set = NISTDB19Dataset(root_dir=G.args.root_dir, data_type=G.args.data_type, train=False, download=True,
-                                   str_classes=G.args.classes, use_preproc=G.args.use_preprocessed_data,
-                                   train_transform=G.train_transform, test_transform=G.test_transform,
-                                   size_limit=G.args.test_limit)
-        if G.args.test_save_path is not None:
-            NISTDB19Dataset.save_to_file(test_set, G.args.test_save_path)
-    else:
-        test_set = NISTDB19Dataset.load_from_file(G.args.test_load_path)
-    get_data_loaders.test = DataLoader(test_set, batch_size=G.args.batch_size,
-                                       shuffle=G.args.shuffle_test if force_shuffle_test is False else True,
-                                       num_workers=0)
-
-    G.classes = train_set.classes
-    return get_data_loaders.train, get_data_loaders.test
+get_data_loaders = NISTDB19Dataset_data_loaders
 
 
 def train_network(net):
+    C = Config.get_instance()
     train_loader, test_loader = get_data_loaders()
 
-    if os.path.exists(G.path_to_model) and False:
-        net.load_state_dict(torch.load(G.path_to_model))
+    if os.path.exists(C.path_to_model) and False:
+        net.load_state_dict(torch.load(C.path_to_model))
     else:
         optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-        criterion = nn.CrossEntropyLoss()
 
         loss_values = []
         start_time = time.perf_counter()
         log_time = 0.0
         size_to_check_loss = math.floor(len(train_loader) / 5) + 1
-        for epoch in range(G.epoch_num):
+        for epoch in range(C.epoch_num):
             net.train()
             running_loss = 0.0
             for i, data in enumerate(train_loader, 0):
-                inputs, labels = data[0].to(G.device), data[1].to(G.device)
+                inputs, labels = data[0].to(C.device), data[1].to(C.device)
 
                 optimizer.zero_grad()
                 outputs = net(inputs)
-                loss = criterion(outputs, labels)
+                loss = C.criterion(outputs, labels)
                 with torch.no_grad():
                     loss_values.append(loss.item())
-                    G.writer.add_scalar('Loss/train', loss.item(), G.epoch_num * epoch + i)
+                    C.writer.add_scalar('Loss/train', loss.item(), C.epoch_num * epoch + i)
                 loss.backward()
                 optimizer.step()
 
@@ -140,68 +52,56 @@ def train_network(net):
 
             if (len(train_loader) - 1) % size_to_check_loss != size_to_check_loss - 1:
                 log('train_logs', f'[{epoch + 1}, {len(train_loader):3d}] loss: {running_loss / (len(train_loader) - 1):1.3f}')
-            log_time += add_logs_to_tensorboard(G, net, test_loader, epoch)
+            log_time += add_logs_to_tensorboard(net, test_loader, epoch)
             log('train_logs',
                 f'Epoch {epoch + 1}   time: {time.perf_counter() - start_time - log_time:6.0f} seconds'
                 f';      log_time: {log_time:6.0f} seconds', epoch + 1)
 
-            if G.args.m_save_period is not None and epoch % G.args.m_save_period == G.args.m_save_period - 1:
-                save_model(G, net, f'{100 * np.mean(eval_cached(G, net, test_loader, epoch).TPR):3.2f}', epoch + 1)
+            if C.args.m_save_period is not None and epoch % C.args.m_save_period == C.args.m_save_period - 1:
+                save_model(net, f'{100 * np.mean(eval_cached(net, test_loader, epoch).TPR):3.2f}', epoch + 1)
 
-        log_time += add_logs_to_tensorboard(G, net, test_loader, G.epoch_num - 1)
+        log_time += add_logs_to_tensorboard(net, test_loader, C.epoch_num - 1)
         log('train_logs', f'Finished Training {time.perf_counter() - start_time - log_time:6.0f} seconds')
 
 
 def main():
-    parser = ArgParser.get_instance()
-    G.args = parser.parse_args()
-    parser.check_compatibility(G.args)
-
-    setup_global_vars()  # All, except transform
-    G.train_transform = transforms.Compose(
-        [transforms.CenterCrop(112),
-         transforms.RandomCrop(96),
-         transforms.Grayscale(num_output_channels=1),
-         transforms.ToTensor(),
-         transforms.Normalize((0.5,), (0.5,))])
-
-    G.test_transform = transforms.Compose(
-        [transforms.CenterCrop(96),
-         transforms.Grayscale(num_output_channels=1),
-         transforms.ToTensor(),
-         transforms.Normalize((0.5,), (0.5,))])
-
     log('common', f"sys version: {sys.version_info}")
     log('common', f"torch version: {torch.__version__}")
     log('common', f"torchvision version: {torchvision.__version__}")
     log('common', f"alphabet_recogniser version: {alphabet_recogniser.__version__}")
     print(f"{'#' * 40}\n\n")
 
+    C = Config.get_instance()
     train_loader, test_loader = get_data_loaders()
-    net = EngAlphabetRecognizer96(num_classes=len(G.classes))
-    upload_net_graph(G, net, test_loader)
+    net = EngAlphabetRecognizer96(num_classes=len(C.classes))
+    upload_net_graph(net, test_loader)
 
-    if G.args.classes is None:
-        G.args.classes = '{'
-        for el in G.classes.values():
-            G.args.classes += el['chr']+','
-        G.args.classes = G.args.classes[:-1] + '}'
+    if C.args.classes is None:
+        C.args.classes = '{'
+        for el in C.classes.values():
+            C.args.classes += el['chr']+','
+        C.args.classes = C.args.classes[:-1] + '}'
 
-    log('common', f'Classes: {G.args.classes[1:-1]}\n')
-    net.to(G.device)
+    log('common', f'Classes: {C.args.classes[1:-1]}\n')
+    net.to(C.device)
     train_network(net)
 
-    metrics = eval_cached(G, net, test_loader, 0, log_loss=False)
+    metrics = eval_cached(net, test_loader, 0, log_loss=False)
     mean_acc_result = f'Accuracy of the network with ' + \
-                      f'{len(G.classes)} classes ({G.train_size_per_class} el per class) ' + \
-                      f'on {G.epoch_num} epoch: {100 * np.mean(metrics.TPR):3.2f}%'
+                      f'{len(C.classes)} classes ({C.train_size_per_class} el per class) ' + \
+                      f'on {C.epoch_num} epoch: {100 * np.mean(metrics.TPR):3.2f}%'
     log('test_accuracy', mean_acc_result)
-    for idx, target in enumerate(G.classes):
-        log('test_accuracy_per_class', f"Recall of '{G.classes[idx]['chr']}': {100 * metrics.TPR[target]:3.2f}%")
+    for idx, target in enumerate(C.classes):
+        log('test_accuracy_per_class', f"Recall of '{C.classes[idx]['chr']}': {100 * metrics.TPR[target]:3.2f}%")
 
-    save_model(G, net, f'{100 * np.mean(metrics.TPR):3.2f}', G.epoch_num)
-    G.writer.close()
+    save_model(net, f'{100 * np.mean(metrics.TPR):3.2f}', C.epoch_num)
+    C.writer.close()
 
 
 if __name__ == "__main__":
+    parser = ArgParser.get_instance()
+    args = parser.parse_args()
+    parser.check_compatibility(args)
+
+    Config.get_instance().initialize(args)
     main()
--- alphabet_recogniser/utils.py
+++ alphabet_recogniser/utils.py
@@ -1,14 +1,91 @@
-import os, time, itertools
+import sys
 import torch
+import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-from sklearn.metrics import roc_curve, auc
-from torchvision.utils import make_grid
-from scipy import interp
+from datetime import datetime
 
-from alphabet_recogniser.test import eval_cached
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+from alphabet_recogniser.datasets import NISTDB19Dataset
+
+
+class Config:
+    __instance__ = None
+    __is_initialized__ = False
+
+    @staticmethod
+    def get_instance():
+        if Config.__instance__ is None:
+            Config()
+        return Config.__instance__
+
+    def __init__(self):
+        if Config.__instance__ is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            self.args = None
+            self.writer = None
+            self.log_pref = None
+
+            self.train_transform = None
+            self.test_transform = None
+            self.classes = None
+            self.train_size_per_class = None
+            self.test_size_per_class = None
+
+            self.device = None
+            self.criterion = None
+            self.epoch_num = None
+            self.path_to_model = None
+
+            Config.__instance__ = self
+
+    def initialize(self, args):
+        self = Config.__instance__
+        if Config.__is_initialized__:
+            raise Exception("Config was already initialized")
+
+        self.train_transform = transforms.Compose(
+            [transforms.CenterCrop(112),
+             transforms.RandomCrop(96),
+             transforms.Grayscale(num_output_channels=1),
+             transforms.ToTensor(),
+             transforms.Normalize((0.5,), (0.5,))])
+
+        self.test_transform = transforms.Compose(
+            [transforms.CenterCrop(96),
+             transforms.Grayscale(num_output_channels=1),
+             transforms.ToTensor(),
+             transforms.Normalize((0.5,), (0.5,))])
+
+        self.args = args
+        self.train_size_per_class = self.args.train_limit
+        self.test_size_per_class = self.args.test_limit
+
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.criterion = nn.CrossEntropyLoss()
+        self.epoch_num = self.args.e
+        self.path_to_model = 'cifar_net.torchmodel'
+
+        if self.args.t_cm_freq is None:
+            self.args.t_cm_freq = sys.maxsize
+
+        if self.args.t_precision_bar_freq is None:
+            self.args.t_precision_bar_freq = sys.maxsize
+
+        num_classes = len(self.args.classes[1:-1].split(',')) if self.args.classes is not None \
+                 else NISTDB19Dataset.folder_map[self.args.data_type]['len']
+
+        self.log_pref = datetime.now().strftime('%Y_%B%d_%H-%M-%S')
+        self.writer = SummaryWriter(log_dir=f"{self.args.t_logdir}{self.log_pref}"
+                                         f"_e[{self.epoch_num}]"
+                                         f"_c[{num_classes}]"
+                                         f"_tr_s[{self.train_size_per_class if self.train_size_per_class is not None else 'All'}]"
+                                         f"_t_s[{self.test_size_per_class if self.test_size_per_class is not None else 'All'}]")
+
+        Config.is_initialized = True
 
 
 def imshow(img):
@@ -16,209 +93,3 @@ def imshow(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
-
-
-def autolabel(rects, ax, fontsize):
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate(f'{int(height)}',
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    fontsize=fontsize,
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
-
-'''
-Tensorboard logs section
-'''
-def save_model(G, net, acc, epoch):
-    if G.args.m_save_path is None:
-        return
-
-    save_path = os.path.join(
-                       G.args.m_save_path,
-                       f"{G.log_pref}"
-                       f"_acc[{acc}]"
-                       f"_e[{epoch}]"
-                       f"_c[{net.num_classes}]"
-                       f"_tr_s[{G.train_size_per_class if G.train_size_per_class is not None else 'All'}]"
-                       f"_t_s[{G.test_size_per_class if G.test_size_per_class is not None else 'All'}]"
-                       f".model")
-
-    if not os.path.exists(save_path):
-        torch.save(net, save_path)
-
-
-def upload_net_graph(G, net, test_loader):
-    if G.args.t_images is None:
-        with torch.no_grad():
-            net.to('cpu')
-            G.writer.add_graph(net)
-            net.to(G.device)
-            return
-
-    with torch.no_grad():
-        images = iter(test_loader).next()[0][:G.args.t_images]
-        images = images.to('cpu')
-        net.to('cpu')
-        G.writer.add_image('MNIST19 preprocessed samples', make_grid(images))
-        G.writer.add_graph(net, images)
-        net.to(G.device)
-
-
-def add_logs_to_tensorboard(G, net, test_loader, epoch):
-    start_time = time.perf_counter()
-
-    classes = [G.classes[key]['chr'] for key in G.classes]
-    metrics = eval_cached(G, net, test_loader, epoch)
-
-    def can_log(frequence):
-        return (epoch % frequence == 0 and epoch != 0) or (epoch % frequence != 0 and G.epoch_num - 1 == epoch)
-
-    if can_log(G.args.t_cm_freq):
-        log_conf_matrix(G, metrics, classes, epoch)
-
-    if can_log(G.args.t_precision_bar_freq):
-        log_TPR_PPV_F1_bars(G, metrics, classes, epoch)
-
-    if can_log(G.args.t_roc_auc_freq):
-        log_ROC_AUC(G, metrics, classes, epoch)
-
-    return time.perf_counter() - start_time
-
-
-def add_fig_to_tensorboard(G, fig, tag, step, close=True):
-    agg = fig.canvas.switch_backends(FigureCanvasAgg)
-    agg.draw()
-
-    img = np.fromstring(agg.tostring_rgb(), dtype=np.uint8, sep='')
-    img = img.reshape(agg.get_width_height()[::-1] + (3,))
-
-    # Normalize into 0-1 range for TensorBoard(X). Swap axes for newer versions where API expects colors in first dim
-    img = img / 255.0
-    img = np.swapaxes(img, 0, 2)
-    img = np.swapaxes(img, 1, 2)
-
-    G.writer.add_image(tag, img, step)
-    if close:
-        plt.close(fig)
-
-
-# G - global variables
-# M - metrics
-def log_conf_matrix(G, M, classes, step, title='Confusion matrix', tensor_name ='MyFigure/image', normalize=False):
-    cm = M.cm
-    if normalize:
-        cm = cm.astype('float')*10 / cm.sum(axis=1)[:, np.newaxis]
-        cm = np.nan_to_num(cm, copy=True)
-        cm = cm.astype('int')
-
-    np.set_printoptions(precision=2)
-    fig, ax = plt.subplots(figsize=(7, 7), dpi=200, facecolor='w', edgecolor='k')
-    ax.imshow(M.cm, cmap='Oranges')
-
-    tick_marks = np.arange(len(classes))
-    fontsize = 23 - round(len(classes) / (1.5 if len(classes) > 13 else 0.8))
-
-    ax.set_xlabel('Predicted', fontsize=fontsize)
-    ax.set_xticks(tick_marks)
-    c = ax.set_xticklabels(classes, fontsize=fontsize, ha='center')
-    ax.xaxis.set_label_position('bottom')
-    ax.xaxis.tick_bottom()
-
-    ax.set_ylabel('True Label', fontsize=fontsize)
-    ax.set_yticks(tick_marks)
-    ax.set_yticklabels(classes, fontsize=fontsize, va ='center')
-    ax.yaxis.set_label_position('left')
-    ax.yaxis.tick_left()
-    ax.set_title('Confusion matrix')
-
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        ax.text(j, i, format(cm[i, j], 'd') if cm[i,j]!=0 else '.', horizontalalignment="center", fontsize=fontsize, verticalalignment='center', color= "black")
-    fig.set_tight_layout(True)
-    add_fig_to_tensorboard(G, fig, 'confusion_matrix', step)
-
-
-# G - global variables
-# M - metrics
-def log_TPR_PPV_F1_bars(G, M, classes, step):
-    fontsize = 24 - round(len(classes) / (1.5 if len(classes) > 13 else 0.8))
-
-    multiplier = 3
-    total_width = 0.75 * multiplier
-    el_width = total_width / 3
-    x = np.arange(0, len(classes) * multiplier, multiplier)
-    np.set_printoptions(precision=2)
-    fig, ax = plt.subplots(figsize=(11, 7), dpi=200, facecolor='w', edgecolor='k')
-    rects_TPR = ax.bar(x - total_width / 2 + el_width * 0.5, M.TPR * 100, width=el_width, align='center', label='Recall')
-    rects_PPV = ax.bar(x - total_width / 2 + el_width * 1.5, M.PPV * 100, width=el_width, align='center', label='Precision')
-    rects_F1  = ax.bar(x - total_width / 2 + el_width * 2.5, M.F1  * 100, width=el_width, align='center', label='F1 score', color='r')
-
-
-    ax.set_ylim([0, 110])
-    ax.set_xlabel('Classes', fontsize=fontsize + 5)
-    ax.set_ylabel('Percents', fontsize=fontsize + 5)
-    ax.set_xticks(x)
-    ax.set_xticklabels(classes, fontsize=fontsize, ha='center')
-    ax.set_title('Recall / Precision / F1')
-    ax.grid()
-    ax.legend(loc='best')
-
-    autolabel(rects_TPR, ax, fontsize - 3)
-    autolabel(rects_PPV, ax, fontsize - 3)
-    autolabel(rects_F1,  ax, fontsize - 3)
-
-    fig.set_tight_layout(True)
-    add_fig_to_tensorboard(G, fig, 'recall/precision/F1', step)
-
-
-# G - global variables
-# M - metrics
-def log_ROC_AUC(G, M, classes, step):
-    fpr = dict()
-    tpr = dict()
-    interp_tprs = []
-    roc_auc = dict()
-    mean_fpr = np.linspace(0, 1, 100)
-    for i in range(len(classes)):
-        fpr[i], tpr[i], _ = roc_curve(
-            M.pred_list[M.lbl_list == i] == M.lbl_list[M.lbl_list == i],
-            M.prob_list[M.lbl_list == i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
-        interp_tpr = interp(mean_fpr, fpr[i], tpr[i])
-        interp_tpr[0] = 0.0
-        interp_tprs.append(interp_tpr)
-
-    np.set_printoptions(precision=2)
-    fig, ax = plt.subplots(figsize=(9, 7), dpi=200, facecolor='w', edgecolor='k')
-
-    for i in range(len(classes)):
-        ax.plot(fpr[i], tpr[i], label=f"ROC '{classes[i]}' (AUC = {roc_auc[i]:0.2f})", linewidth=0.5, alpha=0.5)
-    ax.plot([0, 1], [0, 1],     label='Chance', color='r', linestyle='--')
-
-    mean_tpr = np.mean(interp_tprs, axis=0)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std([roc_auc[key] for key in roc_auc])
-    ax.plot(mean_fpr, mean_tpr, color='b',
-            label=rf'Mean ROC (AUC = {mean_auc:0.2f} $\pm$ {std_auc:0.2f})', lw=2, alpha=0.8)
-
-    std_tpr = np.std(interp_tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=0.2,
-                    label=r'$\pm$ 1 std. dev.')
-
-    ax.set_xlim([0.0, 1.0])
-    ax.set_ylim([0.0, 1.1])
-    ax.set_xlabel('False Positive Rate')
-    ax.set_ylabel('True Positive Rate')
-    ax.set_title('ROC curves')
-    ax.grid()
-    # ax.legend(loc='lower right')
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-    fig.set_tight_layout(True)
-    add_fig_to_tensorboard(G, fig, 'ROC-AUC', step)
